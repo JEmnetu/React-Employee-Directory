7d2f642798546433c56bd764a847e4e05ca9cf45
Jacob Habtemariam <jacob@Jacobs-MBP.home>
2020-03-14 00:12:56 -0400

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( <div className="footer" style={{textAlign:"center", backgroundColor:"black", color:"white", width:"100vw", marginLeft:-15}}>Jacob Habtemariam</div> );
+}
+ 
+export default Footer;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from '../Footer/Footer.jsx';
 
 
 
@@ -15,9 +16,10 @@ class Table extends Component {
     render() { 
         
         
-        return ( <div className="wrapper container-fluid" style={{backgroundColor:'teal', height:"100vh"}}>{this.props.children}
-          
+        return ( <div className="wrapper container-fluid" style={{backgroundColor:'teal', height:"100vh", overflowY:"scroll"}}>{this.props.children}
+          <Footer />
         </div> );
+        
     }
 }
  
